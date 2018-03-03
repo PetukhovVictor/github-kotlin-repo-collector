538db64eb498523702eefb96371067c9706ce109
Victor Petukhov <i@victor.am>
2018-03-03 22:03:03 +0300

    Runner object refactored

--- src/main/kotlin/org/jetbrains/githubkotlinrepocollector/Runner.kt
+++ src/main/kotlin/org/jetbrains/githubkotlinrepocollector/Runner.kt
@@ -14,34 +14,48 @@ import org.jetbrains.bytecodeparser.Stage as BytecodeStage
 
 
 object Runner {
+    private const val ASSETS_DIRECTORY = "assets"
+    private const val JARS_DIRECTORY = "jars"
+    private const val CLASSES_DIRECTORY = "classes"
+    private const val SOURCES_DIRECTORY = "sources"
+    private const val JSON_EXT = "json"
+
+    fun assetsProcess(reposDirectory: String, username: String, repo: String) {
+        val repoName = "$username/$repo"
+        val repoDirectory = "$reposDirectory/$repoName"
+        val repoDirectoryAssets = "$repoDirectory/$ASSETS_DIRECTORY"
+        val repoDirectoryJars = "$repoDirectoryAssets/$JARS_DIRECTORY"
+
+        DirectoryWalker(repoDirectoryAssets).run {
+            JarExtractor(it, it.parentFile.name).extract()
+            BytecodeRunner.walkAndParse(repoDirectoryJars, it.parentFile, username, repo)
+            it.delete()
+        }
+        Files.move(File("$repoDirectoryJars/$username/$repo/$CLASSES_DIRECTORY").toPath(), File("$repoDirectory/$CLASSES_DIRECTORY").toPath())
+        File(repoDirectoryAssets).deleteRecursively()
+    }
+
+    fun repoDownloadAndProcess(reposDirectory: String, username: String, repo: String) {
+        val repoDownloader = RepoDownloader(reposDirectory)
+        val repoSourcesFilter = RepoSourcesFilter(reposDirectory)
+        val repoName = "$username/$repo"
+
+        repoDownloader.downloadSource(repoName)
+        repoSourcesFilter.filterByKtFiles("$repoName/$SOURCES_DIRECTORY")
+        val assetsCollected = repoDownloader.downloadAssets(repoName)
+
+        if (assetsCollected) {
+            assetsProcess(reposDirectory, username, repo)
+        }
+    }
+
     fun run(repoInfoDirectory: String, reposDirectory: String) {
         val repoInfoNodeReference = object: TypeReference<RepoInfoList>() {}
 
-        JsonFilesReader<RepoInfoList>(repoInfoDirectory, "json", repoInfoNodeReference).run(true) { content: RepoInfoList, file: File ->
-            content.items.forEach itemsLoop@{
+        JsonFilesReader<RepoInfoList>(repoInfoDirectory, JSON_EXT, repoInfoNodeReference).run(true) { content: RepoInfoList, file: File ->
+            content.items.forEach {
                 val repoIdentifier = it.full_name.split("/")
-                val repoDownloader = RepoDownloader(reposDirectory)
-                val repoSourcesFilter = RepoSourcesFilter(reposDirectory)
-                val username = repoIdentifier[0]
-                val repo = repoIdentifier[1]
-                val repoName = "$username/$repo"
-                val repoDirectory = "$reposDirectory/$repoName"
-                val repoDirectoryAssets = "$repoDirectory/assets"
-                val repoDirectoryJars = "$repoDirectoryAssets/jars"
-
-                repoDownloader.downloadSource(repoName)
-                repoSourcesFilter.filterByKtFiles("$repoName/sources")
-                val assetsCollected = repoDownloader.downloadAssets(repoName)
-
-                if (assetsCollected) {
-                    DirectoryWalker(repoDirectoryAssets).run {
-                        JarExtractor(it, it.parentFile.name).extract()
-                        BytecodeRunner.walkAndParse(repoDirectoryJars, it.parentFile, username, repo)
-                        it.delete()
-                    }
-                    Files.move(File("$repoDirectoryJars/$username/$repo/classes").toPath(), File("$repoDirectory/classes").toPath())
-                    File(repoDirectoryAssets).deleteRecursively()
-                }
+                repoDownloadAndProcess(reposDirectory, username = repoIdentifier[0], repo = repoIdentifier[1])
             }
         }
     }
